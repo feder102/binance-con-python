e04634771e95b8aacd7be4e7f8c1acd42f212882
Fede Castiglione <feder102@gmail.com>
2022-04-01 17:00:18 -0300

    primera salida de volumenes funcionando bien

--- volumenes.py
+++ volumenes.py
@@ -14,28 +14,24 @@ tickers = client.get_ticker()
 cantidad_monedas = 3
 
 def get_volumen(tick):
-    return tick.get('volume')
+    a = float(tick.get('volume'))
+    return float(tick.get('volume'))
 
 for tick in tickers:
-    if(len(top_tickers_volume) == 0):
+    if (len(top_tickers_volume) >= cantidad_monedas):
+        if (float(top_tickers_volume[cantidad_monedas-1]['volume']) <= float(tick['volume'])):
+            del top_tickers_volume[cantidad_monedas-1]
+            top_tickers_volume.append(tick)
+            top_tickers_volume.sort(key=get_volumen, reverse=True)
+    else:
         top_tickers_volume.append(tick)
-
-    # sort by name (Ascending order)
-    top_tickers_volume.sort(key=get_volumen,reverse=True)
-    if (len(top_tickers_volume) <= cantidad_monedas):
-        if (float(top_tickers_volume[len(top_tickers_volume)-1]['volume']) <= float(tick['volume'])):
-            if(len(top_tickers_volume) == 0):
-                top_tickers_volume.append(tick)
-            else:
-                print("*****************************************")
-                print(top_tickers_volume, end='\n\n')
-                print(len(top_tickers_volume))
-                print("*****************************************")                
-                top_tickers_volume.append(tick)
-                del top_tickers_volume[len(top_tickers_volume)-2]
+        top_tickers_volume.sort(key=get_volumen, reverse=True)
+            
 
 print("*****************************************")
-print(top_tickers_volume, end='\n\n')
+for tick in top_tickers_volume:
+    print(tick['symbol'], end='\n\n')
+    print(tick['volume'], end='\n\n')
 print("*****************************************")
 # for tick in list_of_tickers:
 #     if (tick['symbol'][-4:] != 'USDT' and tick['symbol'][-4:] != 'BUSD'):
